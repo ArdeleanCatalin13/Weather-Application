cc44054bc4d20612ab1b9fd768c202d8f30c55bf
Ardelean Catalin <catalinardelean2000@gmail.com>
2023-04-06 13:26:08 +0300

    Updating UI

--- script.js
+++ script.js
@@ -11,23 +11,17 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temperature").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed " + speed + " km/h";
+        document.querySelector(".humidity").innerText = humidity + "%";
+        document.querySelector(".wind").innerText = speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + name + "')"
     },
     search: function () {
        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
-
-document.querySelector(".search button").addEventListener("click", function () {
-    weather.search();
-
-});
 
 document.querySelector(".search-bar").addEventListener("keyup", function (event) {
     if(event.key == "Enter") {
