f9e1e7de50909c91a16417b25bceb11c0a2eff66
Ardelean Catalin <catalinardelean2000@gmail.com>
2023-04-07 00:08:13 +0300

    Main page constructed

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const slider = document.querySelector(".hourlyforecast");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
+
+});
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
+
+});
+slider.addEventListener("mousemove", (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2;
+    slider.scrollLeft  = scrollLeft - walk;
+    
+});
+
+
+
 let weather = {
     "apiKey": "aedaa0616d345b9f3724c29de166f186",
     fetchWeather: function (city){
@@ -10,13 +42,14 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".city").innerText =  name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temperature").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = humidity + "%";
         document.querySelector(".wind").innerText = speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?" + description + "')"
     },
     search: function () {
        this.fetchWeather(document.querySelector(".search-bar").value);
